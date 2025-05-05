f062a4e178e7b2281e26004f4cd7b1b9cfe3d34a
InterfaceConsciousness <robert.prentner@amcs.science>
2025-05-05 13:10:27 +0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,8 @@
-
+function checkPassword() {
+    var password = document.getElementById("password").value;
+    if (password === "interface") {
+        window.location.href = "index.html"; // Redirect to main site
+    } else {
+        alert("Incorrect password!");
+    }
+}
